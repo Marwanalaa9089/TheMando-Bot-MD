1a23ebcd5f0a805724db279cd9be07bbe00ff8c8
omar-zack <araabzack@gmail.com>
2024-09-29 10:23:31 +0300

    Update المشتركين.js

--- plugins/المشتركين.js
+++ plugins/المشتركين.js
@@ -1,15 +1,15 @@
 let handler = async (m, { conn, isOwner }) => {
 let vip = global.db.data.users[m.sender].premium
 let prem = Object.entries(global.db.data.users).filter(user => user[1].premium)
-let caption = `🎟️ 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 : 𝙑𝙄𝙋 𝙐𝙎𝙀𝙍𝙎
+let caption = `🎟️ قــائــمــة الــمــشــتــركــيــن VIP
 *╭•·–––––––––––––––––––·•*
-│ *Total : ${prem.length} Usuarios* ${prem ? '\n' + prem.map(([jid], i) => `
-│ *${i + 1}.* ${conn.getName(jid) == undefined ? 'Sin Usuarios' : conn.getName(jid)}
+│ *الــمــجــمــوع : ${prem.length} مستخدم* ${prem ? '\n' + prem.map(([jid], i) => `
+│ *${i + 1}.* ${conn.getName(jid) == undefined ? 'Sin فقط' : conn.getName(jid)}
 │ ${isOwner ? '@' + jid.split`@`[0] : jid}\n│ - - - - - - - - -`.trim()).join('\n') : ''}
-*╰•·–––––––––––––––––––·•*\n\n🎟️ 𝗣 𝗥 𝗘 𝗠 𝗜 𝗨 𝗠 ⇢ ${vip ? '✅' : '❌'}\n${wm}`
+*╰•·–––––––––––––––––––·•*\n\n🎟️ مــشــتــرك ⇢ ${vip ? '✅' : '❌'}\n${wm}`
 await conn.reply(m.chat, caption, m, { mentions: await conn.parseMention(caption) })
-/*await conn.sendButton(m.chat, caption, `🎟️ 𝗣 𝗥 𝗘 𝗠 𝗜 𝗨 𝗠 ⇢ ${vip ? '✅' : '❌'}\n${wm}`, null, [ 
-[`${vip ? '✦ 𝘿𝙄𝙎𝙁𝙍𝙐𝙏𝘼𝙍 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 ✦': '✦ 𝘾𝙊𝙈𝙋𝙍𝘼𝙍 𝙋𝘼𝙎𝙀 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 ✦'}`, `${vip ? '.allmenu': '.pase premium'}`]], m, { mentions: await conn.parseMention(caption) })*/
+await conn.sendButton(m.chat, caption, `🎟️ مــشــتــرك ⇢ ${vip ? '✅' : '❌'}\n${wm}`, null, [ 
+[`${vip ? '✦ الــمــطــوريــن ✦': '✦ اشــتــرك فــي فــلاش بــوت ✦'}`, `${vip ? '.المطور': '.الباقات'}`]], m, { mentions: await conn.parseMention(caption) })
 }
 handler.command = /^(المشتركين|listpremium)$/i
 
