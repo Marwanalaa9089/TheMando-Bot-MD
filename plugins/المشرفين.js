666518f5f3caaa46fbd89a7a781e394f68d6b2fa
omar-zack <araabzack@gmail.com>
2024-10-01 05:01:08 +0300

    Update المشرفين.js

--- plugins/المشرفين.js
+++ plugins/المشرفين.js
@@ -4,16 +4,16 @@ const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 let pesan = args.join` `
-let oi = `*الرساله:* ${pesan}`
-let text = `╮──────「* قائمه المشرفين *」────⟢ـ
+let oi = `الرساله:* ${pesan}`
+let text = `*╮────「قائمه المشرفين」──⟢ـ*
 
-┆${oi}
+*┆${oi}
 
-*الادمن:*
-┆${listAdmin}
+*┆الادمن:*
+${listAdmin}
 
 *[ ⚠ ️] هذا الامر للحالات الطارئه فقط!!*
-╯───────────────────⟢ـ`.trim()
+*╯───────────────────⟢ـ*`.trim()
 conn.sendFile(m.chat, pp, 'Menu.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
 }
 handler.help = ['admins <texto>']
