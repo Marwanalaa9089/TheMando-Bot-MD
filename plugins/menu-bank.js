4d0b592678e48bbab7eaeab36a17270311485be4
omar-zack <araabzack@gmail.com>
2024-10-01 05:00:20 +0300

    Update menu-bank.js

--- plugins/menu-bank.js
+++ plugins/menu-bank.js
@@ -1,35 +1,21 @@
-function clockString(ms) {
-    let h = Math.floor(ms / 3600000);
-    let m = Math.floor(ms % 3600000 / 60000);
-    let s = Math.floor(ms % 60000 / 1000);
-    return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':');
-}
-
 import pkg from '@whiskeysockets/baileys';
-const { generateWAMessageFromContent, proto, prepareWAMessageMedia } = pkg;
+const { prepareWAMessageMedia } = pkg;
 
-const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
-    let d = new Date(new Date + 3600000);
-    let locale = 'ar';
-    let week = d.toLocaleDateString(locale, { weekday: 'long' });
-    let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' });
-    let _uptime = process.uptime() * 1000;
-    let uptime = clockString(_uptime);
-    let user = global.db.data.users[m.sender];
-    let name = conn.getName(m.sender)
-    let { money, joincount } = global.db.data.users[m.sender];
-    let { exp, limit, level, role, health, crystal, upgrader, wood } = global.db.data.users[m.sender];
-    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length;
-    let more = String.fromCharCode(8206);
-    let readMore = more.repeat(850);
+const handler = async (m, { conn }) => {
+    await conn.sendMessage(m.chat, { react: { text: '🏦', key: m.key } });
+
+    const zack = 'https://envs.sh/WDg.jpg';
+    let { exp, limit, level, role, crystal, health, upgrader, wood } = global.db.data.users[m.sender]
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-  await conn.sendMessage(m.chat, { react: { text: '🏦', key: m.key } })
-  const harley = 'https://envs.sh/SKm.jpg'
-  const mentionId = m.key.participant || m.key.remoteJid;
- 
-conn.relayMessage(m.chat, { viewOnceMessage: { message: { interactiveMessage: { header: { title: `harley`}, body: { text: `˼⚡˹↜ مـࢪحـبـا بـك/ي @${mentionId.split('@')[0]}
-> ˼🏦˹↜ قــســم الــبــنــك ↶
+    let messageContent = {
+        viewOnceMessage: {
+            message: {
+                interactiveMessage: {
+                    header: { title: 'Zack' },
+                    body: {
+                        text: `˼⚡˹↜ مـࢪحـبـا بـك/ي ${taguser}
+> ˼🪪˹↜ مــعــلــومــاتــك  ↶
 ╮───────────────────⟢ـ
 ┆⚜️↜ مـــســـتواك↞⌊ ${level} ⌉
 ┆💫↜ رتـبـتـك↞⌊ ${role} ⌉
@@ -39,91 +25,46 @@ conn.relayMessage(m.chat, { viewOnceMessage: { message: { interactiveMessage: { 
 ┆🧰↜ الــخــبــرة↞⌊ ${upgrader} ⌉
 ┆🪵↜ الــخــشــب↞⌊ ${wood} ⌉
 ╯───────────────────⟢ـ
-> ˼🎗˹↜ اضغط الزر ↶
-> © 𝐍𝐀𝐑𝐔𝐓𝐎 & 𝐙𝐀𝐂𝐊 2025`,subtitle: "zack",},header: { hasMediaAttachment: true,...(await prepareWAMessageMedia({ image : { url: harley } }, { upload: conn.waUploadToServer }, {quoted: m}))},
-                    contextInfo: {
-                        mentionedJid: [m.sender],
-                        isForwarded: false,
-                    },nativeFlowMessage: { buttons: [
-
-
+> ˼🏦˹↜  قــســم الــبــنـك↶
+╮───────────────────⟢ـ
+┆˼🏦˹┆⌊بنك⌉
+┆˼🏦˹┆⌊سحب⌉
+┆˼🏦˹┆⌊ايداع⌉
+┆˼🏦˹┆⌊اسبوعي⌉
+┆˼🏦˹┆⌊عملاتي⌉
+┆˼🏦˹┆⌊محفظة⌉
+┆˼🏦˹┆⌊عملات⌉
+┆˼🏦˹┆⌊تفعيل⌉
+╯───────────────────⟢ـ
+> © 𝐍𝐀𝐑𝐔𝐓𝐎 & 𝐙𝐀𝐂𝐊 2025`,
+                        subtitle: "HARLEY"
+                    },
+                    header: {
+                        hasMediaAttachment: true,
+                        ...(await prepareWAMessageMedia({ image: { url: zack } }, { upload: conn.waUploadToServer }, { quoted: m }))
+                    },
+                    nativeFlowMessage: {
+                        buttons: [
                             {
-                                name: 'single_select',
-                                buttonParamsJson: JSON.stringify({
-                                    title: '⌈🏦╎𝑩𝒂𝒏𝒌╎🏦⌋',
-                                    sections: [
-                                        {
-                                            title: 'مــرحـ🛡ـبــا بــك فـي مــ☑هــام فــلاش بـ🤖ـوت',
-                                            highlight_label: '𝒁𝒂𝒄𝒌 3𝒎𝒌',
-                                            rows: [
-                                                {
-                                                    header: 'الــبــنــك',
-                                                    title: 'استدعاء_امر_بنك #بنك',
-                                                    description: '',
-                                                    id: '.بنك'
-                                                },
-                                                {
-                                                    header: 'الــمــحــفــظــة',
-                                                    title: 'استدعاء_امر_المحفظة #محفظة',
-                                                    description: '',
-                                                    id: '.محفظة'
-                                                },
-                                                {
-                                                    header: 'ايــداع',
-                                                    title: 'استدعاء_ايداع_البنك #ايداع',
-                                                    description: '',
-                                                    id: '.ايداع'
-                                                },
-                                                {
-                                                    header: 'جــوائــزك الاســبــوعــيــة',
-                                                    title: 'استدعاء_الجوائز_الاسبوعية #اسبوعي',
-                                                    description: '',
-                                                    id: '.اسبوعي'
-                                                },
-                                                {
-                                                    header: 'ســحــب',
-                                                    title: 'استدعاء_سحب_من_البنك #سحب',
-                                                    description: '',
-                                                    id: '.سحب'
-                                                },
-                                                {
-                                                    header: 'عــمــلاتي',
-                                                    title: 'استدعاء_عملات_الذهب #عملاتي',
-                                                    description: '',
-                                                    id: '.عملاتي'
-                                                },
-                                                {
-                                                    header: 'عــمــلات',
-                                                    title: 'استدعاء_امر_عملات #عملات',
-                                                    description: '',
-                                                    id: '.عملات'
-                                                },
-                                                {
-                                                    header: 'تــفــعــيــل_الــبــوت',
-                                                    title: 'استدعاء_تسجيل_البوت #تفعيل',
-                                                    description: '',
-                                                    id: '.تفعيل'
-                                               }
-                                            ]
-                                        }
-                                    ]
-                                }),
-                  messageParamsJson: ''
-                     },
-                     {
-               name: "cta_url",
-               buttonParamsJson: '{"display_text":"⌈📲╎قـنـاة الــبــوت╎📲⌋","url":"https://whatsapp.com/channel/0029VaoUBmSKmCPIIiEatx1H","merchant_url":"https://whatsapp.com/channel/0029VaoUBmSKmCPIIiEatx1H"}'
+                                name: "cta_url",
+                                buttonParamsJson: '{"display_text":"⌈🤖╎قـنـاة الــبــوت╎🤖⌋","url":"https://whatsapp.com/channel/0029VaoUBmSKmCPIIiEatx1H","merchant_url":"https://whatsapp.com/channel/0029VaoUBmSKmCPIIiEatx1H"}'
+                            },
+                            {
+                                name: "cta_url",
+                                buttonParamsJson: '{"display_text":"⌈📲╎قـنـاة الـمـطـور╎📲⌋","url":"https://whatsapp.com/channel/0029ValRCUIFi8xmK9xBOr0I","merchant_url":"https://whatsapp.com/channel/0029ValRCUIFi8xmK9xBOr0I"}'
                             }
                         ]
                     }
                 }
             }
         }
-    }, {});
-}
+    };
+
+    conn.relayMessage(m.chat, messageContent, {});
+};
 
 handler.help = ['info'];
 handler.tags = ['main'];
-handler.command = ['بنكك']
+handler.command = ['بنكك', 'bank', 'Bank', 'BANK'];
 
 export default handler;
