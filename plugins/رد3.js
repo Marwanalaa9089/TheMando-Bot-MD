4bc661068cedc65b7b9a3932371f3cdac775c840
Araab Zack <162893646+araab-zack@users.noreply.github.com>
2024-10-05 07:34:45 +0300

    Update رد3.js

--- plugins/رد3.js
+++ plugins/رد3.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `*قــلــب فـلاش مـن جـوا 👀⚡*`;
-await conn.sendMessage(m.chat, { react: { text: '🙌', key: m.key } })
-    conn.sendFile(m.chat, 'https://envs.sh/06H.jpg', 'image.jpg', message, m);
+    let message = `*سـمـعـت حـد بـيـنـادي عـلـيـا 🦇*`;
+await conn.sendMessage(m.chat, { react: { text: '🦇', key: m.key } })
+    conn.sendFile(m.chat, 'https://envs.sh/Bin.jpg', 'image.jpg', message, m);
 };
 
-handler.customPrefix = /^فلاش$/i;
+handler.customPrefix = /^رايزل$/i;
 handler.command = new RegExp;
 
 export default handler;
