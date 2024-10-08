efb87bbc8475bc37c8afbb57c83502985d02db74
omar-zack <araabzack@gmail.com>
2024-10-08 05:29:33 +0300

    Add files via upload

---
+++ plugins/ذكاء_اصطناعي.js
@@ -0,0 +1,23 @@
+import fetch from 'node-fetch'
+
+const handler = async (m, { text,  usedPrefix, command }) => {
+if (!text) return conn.reply(m.chat, 🍟 *اهلا بك انا ${command} ذكاء اصطناعي عليك كتب نص لارد عليك*\n\nمثل, ${usedPrefix + command} من انت ؟, m)
+try {
+let prompt = انت بوت واتساب بي اسم ${command}  و تحدث فقط باللهجة المصرية و استخدم الايمواجي كثيراً و مطورك هو "زاك و ناروتو" أن سالك من صانعك قل له أن من صنعك هو زاك و ناروتو;
+
+await m.react('🕒')
+
+const apii = await fetch(https://artst-three.vercel.app/chatgpt?text=${text}&prompt=${prompt})
+const res = await apii.json()
+await conn.reply(m.chat, res.result, m)
+await m.react('✅️')
+} catch (error) {
+await m.react('✖️')
+console.error(error)
+return conn.reply(m.chat, '🚩 *ايرور*', m)
+}}
+
+handler.command = ['ناروتو','زاك','ايرين','مايكي']
+handler.help = ['Z A C K']
+handler.tags = ['ai'];
+export default handler
