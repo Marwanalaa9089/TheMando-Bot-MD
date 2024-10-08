802e86cf1c42738cb20d3306d00a761e41207793
Araab Zack <162893646+araab-zack@users.noreply.github.com>
2024-10-08 04:43:24 +0300

    Update rpg-leaderboard.js

--- plugins/rpg-leaderboard.js
+++ plugins/rpg-leaderboard.js
@@ -1,65 +1,57 @@
-const handler = async (m, {conn, args, participants}) => {
-  const users = Object.entries(global.db.data.users).map(([key, value]) => {
-    return {...value, jid: key};
-  });
-  const sortedExp = users.map(toNumber('exp')).sort(sort('exp'));
-  const sortedLim = users.map(toNumber('limit')).sort(sort('limit'));
-  const sortedLevel = users.map(toNumber('level')).sort(sort('level'));
-  const usersExp = sortedExp.map(enumGetKey);
-  const usersLim = sortedLim.map(enumGetKey);
-  const usersLevel = sortedLevel.map(enumGetKey);
-  const len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 10)) : Math.min(10, sortedExp.length);
-  const adventurePhrases = [
-  "قيادة المغامرة وشق طريقك إلى القمة.",
-  "¡تحدي المجهول والوصول إلى آفاق جديدة!",
-  "ستقودك شجاعتك إلى قمة لوحة المتصدرين.",
-  "في كل خطوة، قم بنحت أسطورتك في هذه المغامرة الرائعة.",
-  "استكشف وتنافس وأثبت عظمتك في هذا المنتدى.",
-  "كل خطوة مهمة في رحلتك إلى قمة التصنيف العالمي.",
-  "إن إثارة المنافسة تدفعك إلى الأمام.",
-  "المغامرة وقهر الأماكن الأولى بكل عزيمة.",
-];
-  const randomAdventurePhrase = adventurePhrases[Math.floor(Math.random() * adventurePhrases.length)];
-  const texto = `
-*< جدول المغامرين الأكثر تميزًا />*
-    
-—◉ *توب ${len} دولارات 💷*
-*💷 الترتيب :* ╮───────────────────⟢\n${usersExp.indexOf(m.sender) + 1} de ${usersExp.length}/n╯───────────────────⟢
+let handler = async (m, { conn, args, participants }) => {
+let users = Object.entries(global.db.data.users).map(([key, value]) => {
+return {...value, jid: key}})
+let sortedExp = users.map(toNumber('exp')).sort(sort('exp'))
+let sortedLim = users.map(toNumber('cookies')).sort(sort('cookies'))
+let sortedLevel = users.map(toNumber('level')).sort(sort('level'))
+let usersExp = sortedExp.map(enumGetKey)
+let usersLim = sortedLim.map(enumGetKey) 
+let usersLevel = sortedLevel.map(enumGetKey)
+let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
 
-${sortedExp.slice(0, len).map(({jid, exp}, i) => `${i + 1}. ${participants.some((p) => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} exp*`).join`\n`}
+let text = `
+╭───═[ *Top ${len} Cookies 🍪* ]═────⋆
+│╭───────────────···
+││ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
+││ ${sortedLim.slice(0, len).map(({ jid, cookies }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${cookies} 🍪*`).join`\n││ `}
+│╰────────────────···
+╰───────────═┅═──────────
 
-—◉ *توب ${len} الماس 💎*
-*👤 موقعك:* ${usersLim.indexOf(m.sender) + 1} de ${usersLim.length}
+╭───═[ *TOP ${len} XP 💫* ]═────⋆
+│╭───────────────···
+││ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
+││ ${sortedExp.slice(0, len).map(({ jid, exp }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} 💫*`).join`\n││ `}
+│╰────────────────···
+╰───────────═┅═──────────
 
-${sortedLim.slice(0, len).map(({jid, limit}, i) => `${i + 1}. ${participants.some((p) => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} الماس*`).join`\n`}
+╭───═[ *Top ${len} Nivel 📈* ]═────⋆
+│╭───────────────···
+││ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
+││ ${sortedLevel.slice(0, len).map(({ jid, level }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Nivel ${level} 📈*`).join`\n││ `}
+│╰────────────────···
+╰───────────═┅═──────────`.trim()
+m.reply(text, null, { mentions: conn.parseMention(text) })
+}
+handler.help = ['lb']
+handler.tags = ['rpg']
+handler.command = ['ترتيب', 'lb'] 
+handler.group = true;
+handler.register = true
+handler.fail = null
+handler.exp = 0
 
-—◉ *توب ${len} لفل 🎚️*
-*👤 موقعك:* ${usersLevel.indexOf(m.sender) + 1} de ${usersLevel.length}
-
-${sortedLevel.slice(0, len).map(({jid, level}, i) => `${i + 1}. ${participants.some((p) => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *لفل ${level}*`).join`\n`}
-
-*⚔️ ${randomAdventurePhrase} ⚔️*`.trim();
-  conn.sendMessage(m.chat, {text: texto, mentions: conn.parseMention(texto)}, {quoted: m})
-};
-handler.help = ['top'];
-handler.tags = ['xp'];
-handler.command = ['ترتيب', 'lb'];
-handler.fail = null;
-export default handler;
+export default handler
 
 function sort(property, ascending = true) {
-  if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property];
-  else return (...args) => args[ascending & 1] - args[!ascending & 1];
+if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property]
+else return (...args) => args[ascending & 1] - args[!ascending & 1]
 }
-
 function toNumber(property, _default = 0) {
-  if (property) {
-    return (a, i, b) => {
-      return {...b[i], [property]: a[property] === undefined ? _default : a[property]};
-    };
-  } else return (a) => a === undefined ? _default : a;
+if (property) return (a, i, b) => {
+return {...b[i], [property]: a[property] === undefined ? _default : a[property]}
 }
-
+else return a => a === undefined ? _default : a
+}
 function enumGetKey(a) {
-  return a.jid;
+return a.jid
 }
